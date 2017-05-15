eaac7cf150284f29aaf0eabe8085a2f185cc0fca
melszhu <melszhu1@gmail.com>
2017-05-15 14:31:24 -0700

    Revert "Merge branch 'master' into reply"
    This reverts commit d338e1e3743d125d748df596a1ca20b8a0d35a62, reversing
    changes made to 67ab3971f07b842fb61d3e14d95068836ec7fc9f.

--- Hello.java
+++ Hello.java
@@ -1,9 +1,6 @@
 public class Hello{
 	public static void main(String[] args) {
-
 		System.out.print("Hello, Mels can you see this?");
 		System.out.println("yo i see you");
-		System.out.print("tamadesu is going to be great... maybe.");
-
 	}
 }
