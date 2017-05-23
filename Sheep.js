1b1149bbdd232f6df45f2e89f7cc35d33b1ff750
kaixokkiten <kaixokkiten@gmail.com>
2017-05-23 15:33:28 -0700

    works, but whyy?
    no idea, let me know if you find out

--- Sheep.js
+++ Sheep.js
@@ -1,5 +1,9 @@
-$(document).ready(function(){
-    $("#click").click(function(){
-        $("#tama").hide();
-    });
-});
+// $(document).ready(function(){
+//     $("#click").click(function(){
+//         $("#tama").hide();
+//     });    
+// });
+
+function turnpotato(){
+	$("#tama").hide();
+}
