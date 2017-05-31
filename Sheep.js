8a99a57f30e07dbd9b3a8886ace3e07f1033a350
cyntsao <c.tsao17@gmail.com>
2017-05-31 15:43:33 -0700

    added some more features
    a fullness bar, option to change template and pet
    will need to get animations/gifs

--- Sheep.js
+++ Sheep.js
@@ -10,10 +10,6 @@ function disappear(){
 	//alert("press the center button on the game console!") //annoying
 }
 
-// $("#image").click( function(){
-// alert("CLICKED");
-// });
-
 function dosomething() {
 	alert("I DID SOMETHING");
 }
@@ -24,52 +20,84 @@ function change() {
 
 //TAMA
 var pet = {
-	type:"hamtaro",
+	type:"cat",
 	age:0,
-	hunger:0,
-	getAge: function() {
-		return this.age;
-	},
-	getHunger: function() {
-		return this.hunger;
-	}
+	fullness:100
 }
-
 
 function startGame() {
 	$("#start").hide();
-	hungry = setInterval(updateHunger, 500);
-	old = setInterval(updateAge, 1000);
+	hungry = setInterval(updateHunger, 500); //twice per second
+	old = setInterval(updateAge, 1000); //once per second
 }
 
 function updateHunger() {
-	pet.hunger+=1;
-	document.getElementById("hunger").innerHTML = "hunger: " + pet.hunger;
-	if (pet.hunger>=100) {
+	pet.fullness-=1;
+	document.getElementById("fullness_meter").style.width = pet.fullness*2 + "px";
+	if (pet.fullness<=0) {
 		clearInterval(hungry);
 		clearInterval(old);
+		//death animation
+		alert("Your pet has died of starvation.");
 		document.getElementById("status").innerHTML = "status: DEAD [starvation]";
 	}
 }
 
 function feed() {
-	if (pet.hunger<7) {
+	if (pet.fullness>95) {
 		clearInterval(hungry);
 		clearInterval(old);
+		//death animation
+		alert("Your pet has died of overeating.");
 		document.getElementById("status").innerHTML = "status: DEAD [overfed]";
-	} else if (pet.hunger<25) {
-		pet.hunger = 0;
+	} else if (pet.fullness>80) {
+		//feeding animation
+		pet.fullness = 100;
 	} else {
-		pet.hunger-=20;
+		//feeding animation
+		pet.fullness+=20;
 	}
 }
 
 function updateAge() {
 	pet.age+=1;
 	document.getElementById("age").innerHTML = "age: " + pet.age;
-	if (pet.age==20) {
+	if (pet.age==100) {
 		clearInterval(old);
 		clearInterval(hungry);
+		//death animation
+		alert("Your pet has died of old age.");
 		document.getElementById("status").innerHTML = "status: DEAD [old age]";
 	}
 }
+
+//templates
+function template00() {
+	document.getElementById("tama").src = "tamagotchi_base.png";
+}
+function template01() {
+	document.getElementById("tama").src = "tamagotchi_base01.png";
+}
+
+//pet type
+function hamtaro() {
+	pet.type = "hamtaro_01";
+	if (pet.age==0) {
+		document.getElementById("pet").src = pet.type + ".png";
+	}
+}
+function pettype() {
+	pet.type = "typehere";
+	if (pet.age==0) {
+		document.getElementById("pet").src = pet.type + ".png";
+	}
+}
+
+//animations to have
+/*
+	young - idle, feed, death by overeating, starvation, dead sprite
+	adult - idle, feed, death by overeating, starvation, dead sprite
+	elder - idle, feed, death by overeating, starvation, death by old age, dead sprite
+	
+	maybe two-three options for different pets
+*/
