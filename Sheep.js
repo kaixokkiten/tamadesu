14b3ac2b4b44e21169f5e2c2a25bbae32d4b45d3
cyntsao <c.tsao17@gmail.com>
2017-06-05 22:41:34 -0700

    added an additional stat "fun"
    needed a reason for the pet/game buttons. also it can die of boredom now

--- Sheep.js
+++ Sheep.js
@@ -3,6 +3,7 @@ var pet = {
 	type:"blob",
 	age:0,
 	fullness:100,
+	fun:100,
 	alive:"true"
 }
 
@@ -10,6 +11,7 @@ function startGame() {
 	$("#start").hide();
 	hungry = setInterval(updateHunger, 400);
 	old = setInterval(updateAge, 1000); //once per second
+	fun = setInterval(updateFun, 600);
 	console.log(window.innerWidth);
 }
 
@@ -19,6 +21,7 @@ function updateHunger() {
 	if (pet.fullness<=0) {
 		clearInterval(hungry);
 		clearInterval(old);
+		clearInterval(fun);
 		if (pet.alive=="true") {
 			document.getElementById("pet").src = pet.type + "/death.gif";
 			pet.alive="false";
@@ -31,19 +34,22 @@ function feed() {
 	if (pet.fullness>95) {
 		clearInterval(hungry);
 		clearInterval(old);
+		clearInterval(fun);
 		if (pet.alive=="true") {
 			document.getElementById("pet").src = pet.type + "/death.gif";
 			pet.alive="false";
 		}
 		document.getElementById("status").innerHTML = "status: DEAD [overfed]";
 	} else {
-		 document.getElementById("pet").src = pet.type + "/feed.gif";
-		 if (pet.fullness>80) {
-			 pet.fullness = 100;
-		 } else {
-			 pet.fullness+=20;
-		 }
-		 setTimeout(idle, 1000);
+		if (pet.alive=="true") {
+			document.getElementById("pet").src = pet.type + "/feed.gif";
+			if (pet.fullness>80) {
+				pet.fullness = 100;
+			} else {
+				pet.fullness+=20;
+			}
+			setTimeout(idle, 1000);
+		}
 	}
 }
 
@@ -53,6 +59,7 @@ function updateAge() {
 	if (pet.age==100) {
 		clearInterval(old);
 		clearInterval(hungry);
+		clearInterval(fun);
 		if (pet.alive=="true") {
 			document.getElementById("pet").src = pet.type + "/death.gif";
 			pet.alive="false";
@@ -61,9 +68,29 @@ function updateAge() {
 	}
 }
 
+function updateFun() {
+	pet.fun-=1;
+	document.getElementById("fun_meter").style.width = pet.fun*2 + "px";
+	if (pet.fun<=0) {
+		clearInterval(hungry);
+		clearInterval(old);
+		clearInterval(fun);
+		if (pet.alive=="true") {
+			document.getElementById("pet").src = pet.type + "/death.gif";
+			pet.alive="false";
+		}
+		document.getElementById("status").innerHTML = "status: DEAD [bored/unhappy]";
+	}
+}
+
 function petit() {
 	if (pet.alive=="true") {
 		document.getElementById("pet").src = pet.type + "/pet.gif";
+		if (pet.fun>80) {
+			 pet.fun = 100;
+		 } else {
+			 pet.fun+=20;
+		 }
 		setTimeout(idle, 4000);
 	}
 }
